fe9a1b1e76d4886b630f94a6eedd5d467d573092
Yan Araujo <saresende555@gmail.com>
2023-10-24 17:45:10 -0300

    add

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplica(2, 3));
+function dizOla(nome) {
+    return 'Olá' + nome;
+}
+console.log(dizOla('Professor'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplica(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+console.log(multiplica(2, 3));
+
+function dizOla(nome: string) {
+    return 'Olá' + nome;
+}
+
+console.log(dizOla('Professor'));
